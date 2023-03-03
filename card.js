12b2f4242fd4da4c9f5b3c8a6c1362f01e5f48af
sakibbiswas <sakibsakib99880@gmail.com>
2023-03-03 12:35:59 +0600

    date added

--- card.js
+++ card.js
@@ -6,7 +6,18 @@ const loadcards = async () => {
    displaycards(data.data.tools);
 }
 const displaycards = cards => {
-    const containercard = document.getElementById('card-container')
+  const containercard = document.getElementById('card-container')
+  const showbtn = document.getElementById('btnshow');
+     if (cards.length > 6) {
+       cards = cards.slice(0,6)
+        showbtn.classList.remove('d-none')
+        
+    }
+    else {
+        showbtn.classList.add('d-none')
+  }
+  // vhvh
+   
   cards.forEach(card => {
       console.log(card)
         const cardiv = document.createElement('div')
@@ -25,14 +36,30 @@ const displaycards = cards => {
     <hr class="w-100 border-3 ">
     <h5 class="card-title px-3">${card.name}</h5>
     
-        <p class="card-text"></p>
+        <p class="card-text p-3"><i class="fa-regular fa-calendar-days"></i> ${card.published_in}</p>
+        <p class="text-center"><i class="fa-sharp fa-solid fa-arrow-right"></i></p>
       </div>
     </div>
         
         
         `;
         containercard.appendChild(cardiv)
-    })
+  })
+  toggolloader(true)
+ 
 }
+
+const toggolloader = (isloading) => {
+    const loadersection = document.getElementById('loader')
+    if (isloading) {
+        loadersection.classList.remove('d-none')
+    }
+    else {
+        loadersection.classList.add('d-none')
+  }
+  
+}
+
 loadcards()
+ toggolloader(false)
 
